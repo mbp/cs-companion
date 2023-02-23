ddf4ccf7cdb4a2f32106b1494efa83979e2ff8f8
Morten Brix Pedersen <morten@wtf.dk>
2023-02-23 21:23:19 +0100

    Copy to clipboard

--- app/src/components/Maps.vue
+++ app/src/components/Maps.vue
@@ -10,6 +10,20 @@ const goMap = (mapName: string) => {
     params: { mapName: mapName },
   });
 };
+
+const copyCmd1 = async () => {
+  const text = document.getElementById("perfect1")?.innerText;
+  if (text) {
+    await navigator.clipboard.writeText(text);
+  }
+};
+
+const copyCmd2 = async () => {
+  const text = document.getElementById("perfect2")?.innerText;
+  if (text) {
+    await navigator.clipboard.writeText(text);
+  }
+};
 </script>
 
 <template>
@@ -28,10 +42,12 @@ const goMap = (mapName: string) => {
 
   <p class="footer">Perfect practice config</p>
 
-  <pre>
+  <button @click="copyCmd1">Copy to clipboard (#1)</button>
+  <pre id="perfect1">
 sv_cheats 1;bot_kick;mp_limitteams 0;mp_autoteambalance 0;mp_maxmoney 60000;mp_startmoney 60000;mp_buytime 9999;mp_buy_anywhere 1;mp_freezetime 0;mp_roundtime 60;mp_roundtime_defuse 60;mp_respawn_on_death_ct 1;mp_respawn_on_death_t 1;sv_infinite_ammo 1
   </pre>
-  <pre>
+  <button @click="copyCmd2">Copy to clipboard (#2)</button>
+  <pre id="perfect2">
 sv_grenade_trajectory 1;sv_grenade_trajectory_time 15;sv_showimpacts 1;sv_showimpacts_time 10;ammo_grenade_limit_total 5;mp_warmup_end;mp_restartgame 1
 
   </pre>
@@ -72,5 +88,14 @@ sv_rethrow_last_grenade
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+pre {
+  height: auto;
+  max-height: 200px;
+  overflow: auto;
+  word-break: normal !important;
+  word-wrap: normal !important;
+  white-space: pre !important;
 }
 </style>
