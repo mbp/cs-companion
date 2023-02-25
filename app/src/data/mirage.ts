91eb6ad2c0c44eff2454b5337f4e1ea677e70cae
Morten Brix Pedersen <morten@wtf.dk>
2023-02-25 09:31:03 +0100

    mirage and inferno

--- app/src/data/mirage.ts
+++ app/src/data/mirage.ts
@@ -6,9 +6,7 @@ export const mirageLineups: MapLineups = {
     {
       name: "Connector smoke from T apps",
       description: "Stand in corner, aim lower mid carpet",
-      resultImage: "mirage/t/smokes/mirage-result-con-smoke-lower-from-t.png",
-      aimImage: "mirage/t/smokes/mirage-aim-con-smoke-lower-from-t.png",
-      posImage: "mirage/t/smokes/mirage-pos-con-smoke-lower-from-t.png",
+      folder: "mirage/t/smokes/connector-lower-from-t",
       ticks: "64",
       throwType: "jump",
       side: "t",
@@ -16,7 +14,7 @@ export const mirageLineups: MapLineups = {
     {
       name: "Top connector smoke from t mid",
       description: "Stand behind boxes, aim below plant",
-      aimImage: "mirage/t/smokes/mirage-aim-top-connector-smoke-from-mid.png",
+      folder: "mirage/t/smokes/top-connector-from-mid",
       ticks: "64",
       throwType: "normal",
       side: "t",
@@ -24,9 +22,7 @@ export const mirageLineups: MapLineups = {
     {
       name: "Stairs smoke from T ramp",
       description: "At small hinge. Aim middle of some lines",
-      resultImage: "mirage/t/smokes/mirage-result-stairs-smoke-from-t-ramp.png",
-      aimImage: "mirage/t/smokes/mirage-aim-stairs-smoke-from-t-ramp.png",
-      posImage: "mirage/t/smokes/mirage-pos-con-smoke-lower-from-t.png",
+      folder: "mirage/t/smokes/stairs-from-t-ramp",
       ticks: "64",
       throwType: "normal",
       side: "t",
@@ -34,15 +30,14 @@ export const mirageLineups: MapLineups = {
     {
       name: "CT car platform",
       description: "Next to under stairs. Aim in the sky",
-      aimImage: "mirage/t/smokes/mirage-aim-ct-car-platform.png",
-      posImage: "mirage/t/smokes/mirage-pos-ct-car-platform.png",
+      folder: "mirage/t/smokes/ct-car-platform",
       ticks: "64",
       throwType: "normal",
       side: "t",
     },
     {
       name: "Mirage CT mid one way smoke",
-      aimImage: "mirage/ct/smokes/mirage-aim-mid-one-way-smoke.png",
+      folder: "mirage/ct/smokes/mid-one-way",
       ticks: "64",
       throwType: "normal",
       side: "ct",
@@ -53,8 +48,7 @@ export const mirageLineups: MapLineups = {
   flashBangs: [
     {
       name: "Pop flash b site",
-      aimImage:
-        "mirage/t/flash/mirage-aim-pop-flash-b-site-from-apartments.png",
+      folder: "mirage/t/flash/pop-flash-b-site-from-apps",
       ticks: "64",
       throwType: "normal",
       side: "t",
