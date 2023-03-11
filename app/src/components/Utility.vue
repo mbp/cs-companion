24cfaa3cb8b54002aa4a6635e17d46d7cf214615
Morten Brix Pedersen <morten@wtf.dk>
2023-03-11 17:04:07 +0100

    extract info box

--- app/src/components/Utility.vue
+++ app/src/components/Utility.vue
@@ -4,6 +4,7 @@ import { useRoute } from "vue-router";
 import { allUtilities } from "../data";
 import Navigation from "./Navigation.vue";
 import Image from "./Image.vue";
+import UtilityInfo from "./UtilityInfo.vue";
 
 const route = useRoute();
 
@@ -22,10 +23,6 @@ const getImage = (part: string) => {
   }/${utility.value.id}/${part}.png`;
 };
 
-const getSideImage = computed(() => {
-  return utility.value.side === "ct" ? "ct-icon.webp" : "t-icon.webp";
-});
-
 defineExpose({
   getImage,
 });
@@ -39,23 +36,10 @@ defineExpose({
       { routeName: 'Map', title: mapName, params: { mapName: mapName } },
     ]"
   />
-  <h3>
-    <img :src="getSideImage" class="side-icon-header" />
-  </h3>
   <div>
-    <div class="info-area">
-      <div class="info info-type">
-        Throw type:
-        {{ utility.throwType }}
-        Ticks type:
-        {{ utility.ticks }}
-      </div>
-      <div class="info info-description" v-if="utility.description">
-        {{ utility.description }}
-      </div>
-      <div>
-        <Image :src="getImage('result')" :caption="'Result'" />
-      </div>
+    <UtilityInfo :utility="utility" />
+    <div>
+      <Image :src="getImage('result')" :caption="'Result'" />
     </div>
     <div class="aim-pos-images">
       <div>
@@ -72,25 +56,5 @@ defineExpose({
 .aim-pos-images {
   display: grid;
   grid-template-columns: 50% 50%;
-}
-div.info {
-  padding: 2px;
-  width: 50%;
-  margin: auto;
-}
-
-div.info-type {
-  background-color: lightgreen;
-  border: 1px solid black;
-  color: black;
-}
-
-div.info-description {
-  background-color: orange;
-  border: 1px solid black;
-  color: black;
-}
-.side-icon-header {
-  width: 50px;
 }
 </style>
---
+++ app/src/components/UtilityInfo.vue
@@ -0,0 +1,51 @@
+<template>
+  <img :src="getSideImage" class="side-icon-header" />
+  <div class="info-area">
+    <div class="info info-type">
+      Throw type:
+      {{ utility.throwType }}
+      Ticks type:
+      {{ utility.ticks }}
+    </div>
+    <div class="info info-description" v-if="utility.description">
+      {{ utility.description }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { UtilityLineup } from "./composables/types";
+
+interface Props {
+  utility: UtilityLineup;
+}
+const props = defineProps<Props>();
+
+const getSideImage = computed(() => {
+  return props.utility.side === "ct" ? "ct-icon.webp" : "t-icon.webp";
+});
+</script>
+<style scoped>
+div.info {
+  padding: 2px;
+  width: 50%;
+  margin: auto;
+}
+
+div.info-type {
+  background-color: lightgreen;
+  border: 1px solid black;
+  color: black;
+}
+
+div.info-description {
+  background-color: orange;
+  border: 1px solid black;
+  color: black;
+}
+
+.side-icon-header {
+  width: 50px;
+}
+</style>
