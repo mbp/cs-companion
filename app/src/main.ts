bec352ed7b20647e2031369c511cbd833bd13520
Morten Brix Pedersen <morten@wtf.dk>
2023-03-11 16:24:16 +0100

    remove comments

--- app/src/main.ts
+++ app/src/main.ts
@@ -11,9 +11,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
-  routes, // short for `routes: routes`
+  routes,
 });
 
 createApp(App).use(router).mount("#app");
