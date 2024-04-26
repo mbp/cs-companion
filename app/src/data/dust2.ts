5f4f50651d953d0599b912222e0ddd94ab1ce1ec
Morten Brix Pedersen <morten@wtf.dk>
2024-04-26 10:54:58 +0200

    dust2 lineups

--- app/src/data/dust2.ts
+++ app/src/data/dust2.ts
@@ -14,8 +14,68 @@ export const dust2MapScheme: MapScheme = {
         y: 414,
       },
       positionCoordinates: {
-        x: 466,
-        y: 958,
+        x: 446,
+        y: 887,
+      },
+    },
+    {
+      name: "long from t outside",
+      id: "long-from-t-outside",
+      nadeType: "flashbang",
+      throwType: "jump",
+      side: "t",
+      coordinates: {
+        x: 784,
+        y: 604,
+      },
+      positionCoordinates: {
+        x: 650,
+        y: 821,
+      },
+    },
+    {
+      name: "b from upper tunnels",
+      id: "b-from-upper-tunnels",
+      nadeType: "flashbang",
+      throwType: "jump",
+      side: "t",
+      coordinates: {
+        x: 62,
+        y: 300,
+      },
+      positionCoordinates: {
+        x: 198,
+        y: 517,
+      },
+    },
+    {
+      name: "mid from xbox",
+      id: "mid-from-xbox",
+      nadeType: "flashbang",
+      throwType: "normal",
+      side: "t",
+      coordinates: {
+        x: 462,
+        y: 362,
+      },
+      positionCoordinates: {
+        x: 503,
+        y: 422,
+      },
+    },
+    {
+      name: "b doors from upper tunnels",
+      id: "b-doors-from-upper-tunnels",
+      nadeType: "smoke",
+      throwType: "normal",
+      side: "t",
+      coordinates: {
+        x: 264,
+        y: 231,
+      },
+      positionCoordinates: {
+        x: 142,
+        y: 511,
       },
     },
   ],
