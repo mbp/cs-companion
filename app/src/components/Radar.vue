3cc720a57490b95568cbc83317dcdc4188981b91
Morten Brix Pedersen <morten@wtf.dk>
2024-05-04 18:22:29 +0200

    improved clipboard copy

--- app/src/components/Radar.vue
+++ app/src/components/Radar.vue
@@ -32,7 +32,10 @@ const getPosition = (event: MouseEvent) => {
 const clickRadar = (event: MouseEvent) => {
   const { x, y } = getPosition(event);
   if (import.meta.env.DEV) {
-    console.log("x,y", x, y);
+    const text = `x: ${x},
+        y: ${y},`;
+    console.log("copied to clipboard", text);
+    navigator.clipboard.writeText(text);
   }
 
   emit("radarClick", x, y);
