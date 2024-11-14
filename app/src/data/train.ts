25accc9ad468d237967789aa758ba74c45e1ec39
Morten Brix Pedersen <morten@wtf.dk>
2024-11-14 20:19:26 +0100

    train smokes

--- app/src/data/train.ts
+++ app/src/data/train.ts
@@ -2,6 +2,37 @@ import { MapScheme } from "../components/types";
 
 export const trainMapScheme: MapScheme = {
   map: "Train",
-  lineUps: [],
+  lineUps: [
+    {
+      name: "Ivy from alley",
+      id: "ivy-from-alley",
+      nadeType: "smoke",
+      throwType: "jump",
+      side: "t",
+      coordinates: {
+        x: 729.5,
+        y: 462,
+      },
+      positionCoordinates: {
+        x: 172.5,
+        y: 144,
+      },
+    },
+    {
+      name: "Z from from alley",
+      id: "z-from-alley",
+      nadeType: "smoke",
+      throwType: "normal",
+      side: "t",
+      coordinates: {
+        x: 588.5,
+        y: 557,
+      },
+      positionCoordinates: {
+        x: 447.5,
+        y: 73,
+      },
+    },
+  ],
   strats: [],
 };
