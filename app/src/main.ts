9e7644f09288cd67017176ef37d3c6aefe3bb6ed
Morten Brix Pedersen <morten@wtf.dk>
2024-12-30 14:11:26 +0100

    adding titles

--- app/src/main.ts
+++ app/src/main.ts
@@ -13,27 +13,63 @@ import Strategy from "./components/strategy/Strategy.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/utility", name: "MapsUtility", component: MapsUtility },
-  { path: "/utility/:mapName", component: MapUtility, name: "MapUtility" },
+  { path: "/", name: "Home", component: Home, meta: { title: "Home " } },
+  {
+    path: "/utility",
+    name: "MapsUtility",
+    component: MapsUtility,
+    meta: { title: "Utility " },
+  },
+  {
+    path: "/utility/:mapName",
+    component: MapUtility,
+    name: "MapUtility",
+    meta: { title: "Utility - Map" },
+  },
   {
     path: "/utility/:mapName/:nadeType/:id",
     component: Lineup,
     name: "Lineup",
+    meta: { title: "Lineup" },
   },
-  { path: "/strats", name: "MapsStrats", component: MapsStrats },
-  { path: "/strats/:mapName", component: MapStrats, name: "MapStrats" },
+  {
+    path: "/strats",
+    name: "MapsStrats",
+    component: MapsStrats,
+    meta: { title: "Strats " },
+  },
+  {
+    path: "/strats/:mapName",
+    component: MapStrats,
+    name: "MapStrats",
+    meta: { title: "Strats - Map" },
+  },
   {
     path: "/strats/:mapName/:id",
     component: Strategy,
     name: "Strategy",
+    meta: { title: "Strategy" },
   },
-  { path: "/commands", name: "Commands", component: Commands },
+  {
+    path: "/commands",
+    name: "Commands",
+    component: Commands,
+    meta: { title: "Commands " },
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = `${to.meta.title} - CS Companion`;
+  } else {
+    document.title = `CS Companion`;
+  }
+  next();
 });
 
 createApp(App).use(router).mount("#app");
