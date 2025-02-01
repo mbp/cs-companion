bee00a22effafe9989b37baab97ba84b80e23532
Morten Brix Pedersen <morten@wtf.dk>
2025-02-01 19:49:34 +0100

    improved svgs

--- app/src/inline-assets/utility.ts
+++ app/src/inline-assets/utility.ts
@@ -27,19 +27,8 @@ const flashbangSvg: Svg = {
   paths: [
     {
       content: `
-        M32,2
-        a30,30 0 1,0 0,60
-        a30,30 0 1,0 0,-60
-        M30,15
-        h4
-        v30
-        h-4
-        z
-        M28,10
-        h8
-        v5
-        h-8
-        z
+M32 2 A30 30 0 1 1 31.9 2 Z 
+M26 16 L38 16 L31 30 L42 30 L24 48 L31 34 L20 34 Z
       `,
       fill: "white",
       invertedFill: "lightblue",
@@ -52,12 +41,10 @@ const fragGrenadeSvg: Svg = {
   paths: [
     {
       content: `
-        M32,2
-        a30,30 0 1,0 0,60
-        a30,30 0 1,0 0,-60
-        M32,20
-        a10,10 0 1,0 0,20
-        a10,10 0 1,0 0,-20
+M30,2
+a30,30 0 1,0 0,60
+a30,30 0 1,0 0,-60
+M19,30 L22,20 L24,26 L26,20 L30,24 L34,20 L36,26 L38,20 L41,30 L38,34 L42,38 L36,38 L34,42 L36,46 L30,42 L28,46 L26,42 L22,46 L20,42 L18,38 L16,34 Z
       `,
       fill: "green",
       invertedFill: "lightgreen",
