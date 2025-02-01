dfb2f9f910ab012321cb331e39ac2b7f0b373e8d
Morten Brix Pedersen <morten@wtf.dk>
2025-02-01 13:09:03 +0100

    setpos for all ancient lineupes

--- app/src/data/maps/ancient/lineups.ts
+++ app/src/data/maps/ancient/lineups.ts
@@ -32,6 +32,7 @@ export const ancientLineups = [
       x: 823,
       y: 714,
     },
+    setPos: "setpos 1255.968750 -1479.968750 85.202332;setang -11.108881 109.479271 0.000000"
   },
   {
     name: "Cave from ruins",
@@ -47,6 +48,7 @@ export const ancientLineups = [
       x: 836,
       y: 732,
     },
+    setPos: "setpos 1255.968750 -1479.968384 85.202209;setang -11.161748 110.704041 0.000000"
   },
   {
     name: "B ramp from long",
@@ -62,6 +64,7 @@ export const ancientLineups = [
       x: 842,
       y: 382,
     },
+    setPos: "setpos 1343.224487 516.031555 193.404419;setang 6.816636 -95.316444 0.000000"
   },
   {
     name: "B door from long",
@@ -93,6 +96,7 @@ export const ancientLineups = [
       x: 461,
       y: 182,
     },
+    setPos: "setpos -646.968262 1308.031250 94.078506;setang -44.769230 -98.273331 0.00000"
   },
   {
     name: "Elbow from spawn left side",
@@ -108,6 +112,7 @@ export const ancientLineups = [
       x: 570,
       y: 176,
     },
+    setPos: "setpos -66.043579 1290.032837 94.725143;setang -36.796463 -112.951561 0.000000"
   },
   {
     name: "Mid b short/long from t spawn",
@@ -125,6 +130,7 @@ export const ancientLineups = [
       x: 514,
       y: 836,
     },
+    setPos: "setpos -391.964966 -2043.400146 -90.060593;setang -41.368855 56.184380 0.000000"
   },
   {
     name: "Flash right side mid from t spawn",
@@ -140,6 +146,8 @@ export const ancientLineups = [
       x: 545,
       y: 822,
     },
+    setPos:
+      "setpos -216.035156 -1935.092651 -104.281281;setang -22.275923 95.164734 0.000000",
   },
   {
     name: "Flash left side mid from t spawn",
@@ -155,6 +163,7 @@ export const ancientLineups = [
       x: 442,
       y: 840,
     },
+    setPos: "setpos -735.958130 -1944.346680 -102.665710;setang -24.499111 85.050293 0.000000"
   },
   {
     name: "Catwalk from t spawn",
@@ -170,6 +179,7 @@ export const ancientLineups = [
       x: 514,
       y: 843,
     },
+    setPos: "setpos -391.956299 -2055.315918 -91.128769;setang -19.905575 82.886139 0.000000"
   },
   {
     name: "Mid from t spawn",
@@ -185,6 +195,8 @@ export const ancientLineups = [
       x: 514,
       y: 843,
     },
+    setPos:
+      "setpos -391.942139 -2055.482422 -91.128769;setang -25.153559 92.508904 0.000000",
   },
   {
     name: "Inside donut from stairs",
@@ -200,6 +212,8 @@ export const ancientLineups = [
       x: 201,
       y: 586,
     },
+    setPos:
+      "setpos -1968.169922 -815.564148 60.745026;setang -39.911335 75.320862 0.000000",
   },
   {
     name: "CT from stairs",
@@ -215,6 +229,8 @@ export const ancientLineups = [
       x: 201,
       y: 586,
     },
+    setPos:
+      "setpos -1968.169922 -815.564148 60.745026;setang -41.627346 64.972084 0.000000",
   },
   {
     name: "Temple from stairs",
@@ -230,6 +246,8 @@ export const ancientLineups = [
       x: 201,
       y: 586,
     },
+    setPos:
+      "setpos -1968.028076 -808.727173 64.085205;setang -53.296276 79.228104 0.000000",
   },
   {
     name: "Donut mid from spawn",
@@ -245,6 +263,7 @@ export const ancientLineups = [
       x: 514,
       y: 843,
     },
+    setPos: "setpos -391.968994 -2058.811035 -91.128754;setang -26.611124 103.224037 0.000000"
   },
   {
     name: "Donut mid from jungle",
@@ -260,6 +279,7 @@ export const ancientLineups = [
       x: 363,
       y: 676,
     },
+    setPos: "setpos -1114.281128 -1169.243774 52.691795;setang -49.816628 85.034355 0.000000"
   },
   {
     name: "B backline from ruins",
@@ -275,6 +295,7 @@ export const ancientLineups = [
       x: 833,
       y: 732,
     },
+    setPos: "setpos 1255.968994 -1479.968750 85.202408;setang -13.833216 107.015770 0.000000"
   },
   {
     name: "Default from zigzag",
@@ -290,6 +311,7 @@ export const ancientLineups = [
       x: 714,
       y: 573,
     },
+    setPos: "setpos 608.031250 -677.614502 170.866028;setang -34.425339 81.989380 0.000000"
   },
   {
     name: "Site from b door",
@@ -305,6 +327,7 @@ export const ancientLineups = [
       x: 793,
       y: 622,
     },
+    setPos: "setpos 1058.147217 -935.966309 67.368248;setang -4.144608 106.013855 0.000000"
   },
   {
     name: "Wood from ruins",
@@ -320,6 +343,7 @@ export const ancientLineups = [
       x: 791,
       y: 633,
     },
+    setPos: "setpos 1052.079834 -889.462158 72.871246;setang -29.805296 134.922928 0.000000"
   },
   {
     name: "Default from ruins",
@@ -335,6 +359,7 @@ export const ancientLineups = [
       x: 820,
       y: 699,
     },
+    setPos: "setpos 1160.999878 -1321.817383 123.871246;setang 2.693648 103.825607 0.000000"
   },
   {
     name: "Sun room from b door",
@@ -351,6 +376,7 @@ export const ancientLineups = [
       x: 807,
       y: 622,
     },
+    setPos: "setpos 1100.338257 -930.625183 67.368256;setang -8.209345 129.387634 0.000000",
     hasVideo: true,
   },
 ] satisfies UtilityLineup[];
