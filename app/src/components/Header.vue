e6d65f076515702199d4d0211c0a048b9b1252aa
Morten Brix Pedersen <morten@wtf.dk>
2025-02-02 21:28:56 +0100

    smaller padding

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -115,7 +115,7 @@ const breadcrumbs = computed(() => {
           v-if="isSearchActive"
           v-model="searchQuery"
           type="text"
-          class="bg-gray-700 text-white rounded-full px-4 py-2 focus:outline-hidden focus:ring-2 focus:ring-gray-600"
+          class="bg-gray-700 text-white rounded-full px-4 py-1 focus:outline-hidden focus:ring-2 focus:ring-gray-600"
           placeholder="Search..."
         />
         <ul
