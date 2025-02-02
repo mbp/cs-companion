50c54b3b7ec7aa779a5ccfa3527ac794eec0e58d
Morten Brix Pedersen <morten@wtf.dk>
2025-02-02 18:59:38 +0100

    overpass cleanup, thirdperson pos and improved images

--- app/src/data/maps/overpass/lineups.ts
+++ app/src/data/maps/overpass/lineups.ts
@@ -15,6 +15,8 @@ export const overpassLineups = [
       x: 629,
       y: 480,
     },
+    setPos:
+      "setpos -1559.968018 -729.448730 108.107445;setang -30.914227 98.988388 0.000000",
   },
   {
     name: "Mid toilet smoke from t spawn",
@@ -30,6 +32,8 @@ export const overpassLineups = [
       x: 847,
       y: 807,
     },
+    setPos:
+      "setpos -418.031250 -2395.468750 219.871246;setang -23.047026 148.531662 0.000000",
   },
   {
     name: "Dumpster from divider",
@@ -45,6 +49,8 @@ export const overpassLineups = [
       x: 469,
       y: 512,
     },
+    setPos:
+      "setpos -2335.968750 -863.835144 456.319611;setang -1.293424 79.424911 0.000000",
   },
   {
     name: "Dumpster from long",
@@ -60,6 +66,8 @@ export const overpassLineups = [
       x: 195,
       y: 503,
     },
+    setPos:
+      "setpos -3771.267090 -1003.890442 564.463135;setang -16.816307 47.586174 0.000000",
   },
   {
     name: "Barrels molo from short",
@@ -75,6 +83,8 @@ export const overpassLineups = [
       x: 595,
       y: 477,
     },
+    setPos:
+      "setpos -1751.840088 -671.968750 195.871246;setang 7.524358 49.539692 0.000000",
   },
   {
     name: "Short tunnel molo from abc",
@@ -90,22 +100,8 @@ export const overpassLineups = [
       x: 545,
       y: 323,
     },
-  },
-  {
-    name: "Short tunnel molo from A short",
-    description: "Throw when leaving box",
-    id: "short-tunnel-from-a-short",
-    nadeType: "molo",
-    throwType: "run",
-    side: "ct",
-    coordinates: {
-      x: 684,
-      y: 523,
-    },
-    positionCoordinates: {
-      x: 484,
-      y: 321,
-    },
+    setPos:
+      "setpos -1981.733276 124.628616 195.871246;setang -12.170014 -52.725872 0.000000",
   },
   {
     name: "Monster from A site",
@@ -121,6 +117,8 @@ export const overpassLineups = [
       x: 522,
       y: 150,
     },
+    setPos:
+      "setpos -2116.466553 993.139954 547.871216;setang -15.232837 -50.622692 0.000000",
   },
   {
     name: "Playground from long",
@@ -136,6 +134,8 @@ export const overpassLineups = [
       x: 305,
       y: 383,
     },
+    setPos:
+      "setpos -3232.029053 -214.938843 586.621277;setang 3.326205 -83.914314 0.000000",
   },
   {
     name: "T stairs molo from mid",
@@ -152,6 +152,8 @@ export const overpassLineups = [
       x: 476,
       y: 313,
     },
+    setPos:
+      "setpos -2379.778809 140.791595 503.035461;setang 0.606866 -73.909103 0.000000",
   },
   {
     name: "Barrels flash from short tunnel",
@@ -167,6 +169,8 @@ export const overpassLineups = [
       x: 801,
       y: 553,
     },
+    setPos:
+      "setpos -612.163879 -1076.645630 100.817078;setang -30.228069 98.985016 0.000000",
   },
   {
     name: "Bank from long",
@@ -182,6 +186,8 @@ export const overpassLineups = [
       x: 195,
       y: 503,
     },
+    setPos:
+      "setpos -3775.832520 -1003.883911 565.679016;setang -12.803694 57.802864 0.000000",
   },
   {
     name: "Bank from divider",
@@ -197,5 +203,7 @@ export const overpassLineups = [
       x: 469,
       y: 512,
     },
+    setPos:
+      "setpos -2335.968506 -863.683167 456.318237;setang -9.927073 91.020630 0.000000",
   },
 ] satisfies UtilityLineup[];
