ff0a14312d5a1500fd2ed21f36445f84dde2a1c0
Morten Brix Pedersen <morten@wtf.dk>
2025-02-02 17:59:20 +0100

    add magnifying glass

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -6,6 +6,10 @@ import { allMapSchemes } from "../data";
 var router = useRouter();
 
 const searchQuery = ref("");
+const isSearchActive = ref(false);
+const toggleSearch = () => {
+  isSearchActive.value = !isSearchActive.value;
+};
 
 const filteredMaps = computed(() => {
   if (!searchQuery.value) return [];
@@ -102,7 +106,13 @@ const breadcrumbs = computed(() => {
         >
       </div>
       <div class="relative">
+        <span
+          class="text-gray-300 hover:text-white cursor-pointer"
+          @click="toggleSearch"
+          >🔍</span
+        >
         <input
+          v-if="isSearchActive"
           v-model="searchQuery"
           type="text"
           class="bg-gray-700 text-white rounded-full px-4 py-2 focus:outline-hidden focus:ring-2 focus:ring-gray-600"
