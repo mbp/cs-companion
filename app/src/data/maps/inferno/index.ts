071c79870908fa9ca8bdf0c203c62983ff04ba5f
Morten Brix Pedersen <morten@wtf.dk>
2025-02-03 19:05:20 +0100

    adding pistol inferno strat

--- app/src/data/maps/inferno/index.ts
+++ app/src/data/maps/inferno/index.ts
@@ -1,9 +1,10 @@
 import { MapScheme } from "../../../components/types";
 import { infernoLineups } from "./lineups";
+import { infernoStrats } from "./strats";
 
 export const infernoMapScheme: MapScheme = {
   map: "Inferno",
   activeDuty: true,
   lineUps: infernoLineups,
-  strats: [],
+  strats: infernoStrats,
 };
---
+++ app/src/data/maps/inferno/strats.ts
@@ -0,0 +1,12 @@
+import { Strategy } from "../../../components/strategy/types";
+
+export const infernoStrats = [
+  {
+    id: "oldboys-push-long-to-b",
+    name: "Oldboys push long to B",
+    description:
+      "4 players run second mid and directly to long. At the same time, short has been smoked off. Continue running through CT spawn and to B for a plan. Lurk player tries to catch anyone rotating in banana.",
+    side: "t",
+    buyType: "pistols",
+  },
+] satisfies Strategy[];
