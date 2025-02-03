4219783199288994d2c1f523ed6f7e474293c0d4
Morten Brix Pedersen <morten@wtf.dk>
2025-02-03 18:50:45 +0100

    adding mirage strats

--- app/src/data/maps/mirage/index.ts
+++ app/src/data/maps/mirage/index.ts
@@ -1,8 +1,9 @@
 import { MapScheme } from "../../../components/types";
 import { mirageLineups } from "./lineups";
+import { mirageStrats } from "./strats";
 
 export const mirageMapScheme: MapScheme = {
   map: "Mirage",
   lineUps: mirageLineups,
-  strats: [],
+  strats: mirageStrats,
 };
---
+++ app/src/data/maps/mirage/strats.ts
@@ -0,0 +1,12 @@
+import { Strategy } from "../../../components/strategy/types";
+
+export const mirageStrats = [
+  {
+    id: "pistol-late-a-site-crunch",
+    name: "Pistol late A site crunch",
+    description:
+      "4 players walk to under. 1 player on T ramp smokes jungle and short. Then 4 players push a site and ramp player pushes A site as well",
+    side: "t",
+    buyType: "pistols",
+  },
+] satisfies Strategy[];
