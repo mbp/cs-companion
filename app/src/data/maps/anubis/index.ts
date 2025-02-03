05c571de3967b0b8e92661cebe388950946723f1
Morten Brix Pedersen <morten@wtf.dk>
2025-02-03 18:54:50 +0100

    adding anubis strats

--- app/src/data/maps/anubis/index.ts
+++ app/src/data/maps/anubis/index.ts
@@ -1,8 +1,9 @@
 import { MapScheme } from "../../../components/types";
 import { anubisLineups } from "./lineups";
+import { anubisStrats } from "./strats";
 
 export const anubisMapScheme: MapScheme = {
   map: "Anubis",
   lineUps: anubisLineups,
-  strats: [],
+  strats: anubisStrats,
 };
