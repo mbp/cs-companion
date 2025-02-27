9cc5f38446ab88f036c9af95b96b971d40675055
Morten Brix Pedersen <morten@wtf.dk>
2025-02-27 18:32:43 +0100

    improved callouts inferno

--- app/src/data/maps/inferno/callouts.ts
+++ app/src/data/maps/inferno/callouts.ts
@@ -4,23 +4,37 @@ export const infernoCallouts = [
   {
     name: "T spawn",
     coordinates: {
-      x: 75.984375,
+      x: 75,
       y: 686,
     },
   },
   {
     name: "Graveyard",
     coordinates: {
-      x: 922.984375,
+      x: 922,
       y: 707,
     },
   },
   {
     name: "Banana",
     coordinates: {
-      x: 468.984375,
+      x: 468,
       y: 480,
     },
     angle: -59,
+  },
+  {
+    name: "Coffins",
+    coordinates: {
+      x: 534,
+      y: 146,
+    },
+  },
+  {
+    name: "Mid",
+    coordinates: {
+      x: 547,
+      y: 673,
+    },
   },
 ] satisfies MapCallout[];
