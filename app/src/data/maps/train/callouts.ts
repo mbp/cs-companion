abd599cfcf8bae960c1dc95fa3f7f27d6cf0a289
Morten Brix Pedersen <morten@wtf.dk>
2025-03-01 22:12:58 +0100

    add some train callouts

---
+++ app/src/data/maps/train/callouts.ts
@@ -0,0 +1,39 @@
+import { MapCallout } from "../../../components/types/callout";
+
+export const trainCallouts = [
+  {
+    name: "Ivy",
+    coordinates: {
+      x: 798,
+      y: 225,
+    },
+  },
+  {
+    name: "Hell",
+    coordinates: {
+      x: 661,
+      y: 348,
+    },
+  },
+  {
+    name: "T spawn",
+    coordinates: {
+      x: 91,
+      y: 169,
+    },
+  },
+  {
+    name: "Electric",
+    coordinates: {
+      x: 488,
+      y: 498,
+    },
+  },
+  {
+    name: "Popdog",
+    coordinates: {
+      x: 315,
+      y: 582,
+    },
+  },
+] satisfies MapCallout[];
--- app/src/data/maps/train/index.ts
+++ app/src/data/maps/train/index.ts
@@ -1,11 +1,12 @@
 import { MapScheme } from "../../../components/types";
+import { trainCallouts } from "./callouts";
 import { trainLineups } from "./lineups";
 import { trainStrats } from "./strats";
 
 export const trainMapScheme: MapScheme = {
   map: "Train",
   activeDuty: true,
-  callouts: [],
+  callouts: trainCallouts,
   lineUps: trainLineups,
   strats: trainStrats,
 };
