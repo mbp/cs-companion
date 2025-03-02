13574fbe9986d1571ec42e0c6ad3cbd5cf46bc51
Morten Brix Pedersen <morten@wtf.dk>
2025-03-02 13:27:19 +0100

    adding ancient callouts

---
+++ app/src/data/maps/ancient/callouts.ts
@@ -0,0 +1,150 @@
+import { MapCallout } from "../../../components/types/callout";
+
+export const ancientCallouts = [
+  {
+    name: "Temple",
+    coordinates: {
+      x: 320,
+      y: 162,
+    },
+  },
+  {
+    name: "A site",
+    coordinates: {
+      x: 280,
+      y: 275,
+    },
+  },
+  {
+    name: "B site",
+    coordinates: {
+      x: 726,
+      y: 423,
+    },
+  },
+  {
+    name: "T spawn",
+    coordinates: {
+      x: 460,
+      y: 885,
+    },
+  },
+  {
+    name: "CT spawn",
+    coordinates: {
+      x: 475,
+      y: 119,
+    },
+  },
+  {
+    name: "Elbow",
+    coordinates: {
+      x: 315,
+      y: 544,
+    },
+    arrow: {
+      from: { x: 355, y: 560 },
+      to: { x: 373, y: 592 },
+    },
+  },
+  {
+    name: "Donut",
+    coordinates: {
+      x: 285,
+      y: 475,
+    },
+  },
+  {
+    name: "A main",
+    coordinates: {
+      x: 160,
+      y: 413,
+    },
+  },
+  {
+    name: "Mid",
+    coordinates: {
+      x: 480,
+      y: 525,
+    },
+  },
+  {
+    name: "Boost",
+    coordinates: {
+      x: 865,
+      y: 630,
+    },
+    arrow: {
+      from: { x: 876, y: 620 },
+      to: { x: 826, y: 567 },
+    },
+  },
+  {
+    name: "Cave",
+    coordinates: {
+      x: 656,
+      y: 420,
+    },
+  },
+  {
+    name: "Sun room",
+    coordinates: {
+      x: 600,
+      y: 540,
+    },
+  },
+  {
+    name: "Shelf / heaven",
+    coordinates: {
+      x: 535,
+      y: 675,
+    },
+    arrow: {
+      from: { x: 576, y: 659 },
+      to: { x: 577, y: 590 },
+    },
+  },
+  {
+    name: "Boost",
+    coordinates: {
+      x: 240,
+      y: 398,
+    },
+    arrow: {
+      from: { x: 252, y: 379 },
+      to: { x: 222, y: 346 },
+    },
+  },
+  {
+    name: "Short",
+    coordinates: {
+      x: 705,
+      y: 315,
+    },
+  },
+  {
+    name: "Long",
+    coordinates: {
+      x: 830,
+      y: 301,
+    },
+  },
+  {
+    name: "Red",
+    coordinates: {
+      x: 470,
+      y: 350,
+    },
+  },
+  {
+    name: "Doors",
+    coordinates: {
+      x: 721,
+      y: 685,
+    },
+    arrow: {
+      from: { x: 757, y: 668 },
+      to: { x: 789, y: 633 },
+    },
+  },
+] satisfies MapCallout[];
--- app/src/data/maps/ancient/index.ts
+++ app/src/data/maps/ancient/index.ts
@@ -1,11 +1,12 @@
 import { MapScheme } from "../../../components/types";
+import { ancientCallouts } from "./callouts";
 import { ancientLineups } from "./lineups";
 import { ancientStrats } from "./strats";
 
 export const ancientMapScheme: MapScheme = {
   map: "Ancient",
   activeDuty: true,
-  callouts: [],
+  callouts: ancientCallouts,
   lineUps: ancientLineups,
   strats: ancientStrats,
 };
