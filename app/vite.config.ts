b0af940f9345aed6d8bf53c468f73c2a29be57d6
Morten Brix Pedersen <morten@wtf.dk>
2025-05-05 19:32:55 +0200

    Disable options api for smaller bundle

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,9 @@ import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: false
+  },
   plugins: [
     vue(),
     Icons({
