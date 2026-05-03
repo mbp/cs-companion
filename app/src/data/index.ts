ab38544ececebdcf8152b15999dd581a565b3400
Morten Brix Pedersen <morten@wtf.dk>
2026-05-03 16:08:32 +0200

    adding cache basic asset

--- app/src/data/index.ts
+++ app/src/data/index.ts
@@ -1,6 +1,7 @@
 import { MapScheme } from "../components/types";
 import { ancientMapScheme } from "./maps/ancient";
 import { anubisMapScheme } from "./maps/anubis";
+import { cacheMapScheme } from "./maps/cache";
 import { dust2MapScheme } from "./maps/dust2";
 import { infernoMapScheme } from "./maps/inferno";
 import { italyMapScheme } from "./maps/italy";
@@ -14,6 +15,7 @@ import { vertigoMapScheme } from "./maps/vertigo";
 export const allMapSchemes: MapScheme[] = [
   ancientMapScheme,
   anubisMapScheme,
+  cacheMapScheme,
   dust2MapScheme,
   infernoMapScheme,
   italyMapScheme,
---
+++ app/src/data/maps/cache/index.ts
@@ -0,0 +1,9 @@
+import { MapScheme } from "../../../components/types";
+
+export const cacheMapScheme: MapScheme = {
+  map: "Cache",
+  activeDuty: false,
+  callouts: [],
+  lineUps: [],
+  strats: [],
+};
