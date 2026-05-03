8d55e7a3fc5e93fb55bcc74447d6dd30978506bc
Morten Brix Pedersen <morten@wtf.dk>
2026-05-03 16:28:54 +0200

    new lineup added

--- app/src/data/maps/cache/index.ts
+++ app/src/data/maps/cache/index.ts
@@ -1,9 +1,10 @@
 import { MapScheme } from "../../../components/types";
+import { cacheLineups } from "./lineups";
 
 export const cacheMapScheme: MapScheme = {
   map: "Cache",
   activeDuty: false,
   callouts: [],
-  lineUps: [],
+  lineUps: cacheLineups,
   strats: [],
 };
---
+++ app/src/data/maps/cache/lineups.ts
@@ -0,0 +1,22 @@
+import { UtilityLineup } from "../../../components/utility/types";
+
+export const cacheLineups = [
+  {
+    name: "Tree room from sun room",
+    id: "tree-room-from-sun-room",
+    nadeType: "smoke",
+    clickType: "left+right",
+    throwType: "jump",
+    side: "t",
+    coordinates: {
+      x: 275,
+      y: 754,
+    },
+    positionCoordinates: {
+      x: 480,
+      y: 854,
+    },
+    setPos:
+      "setpos 579.209045 -1449.946899 1676.265625;setang -3.088876 149.317825 0.000000",
+  },
+] satisfies UtilityLineup[];
