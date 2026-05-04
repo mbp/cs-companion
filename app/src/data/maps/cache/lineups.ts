905e519cd4e2d448e4be98937247ff9890929bca
Morten Brix Pedersen <morten@wtf.dk>
2026-05-04 17:54:56 +0200

    criss cross smookes

--- app/src/data/maps/cache/lineups.ts
+++ app/src/data/maps/cache/lineups.ts
@@ -19,4 +19,38 @@ export const cacheLineups = [
     setPos:
       "setpos 579.209045 -1449.946899 1676.265625;setang -3.088876 149.317825 0.000000",
   },
+  {
+    name: "Right mid criss cross",
+    id: "right-mid-criss-cross",
+    nadeType: "smoke",
+    throwType: "normal",
+    side: "t",
+    coordinates: {
+      x: 421,
+      y: 527,
+    },
+    positionCoordinates: {
+      x: 679,
+      y: 552,
+    },
+    setPos:
+      "setpos 1730.680054 208.036133 1676.000000;setang -11.510336 173.954117 0.000000",
+  },
+  {
+    name: "Left mid criss cross",
+    id: "left-mid-criss-cross",
+    nadeType: "smoke",
+    throwType: "normal",
+    side: "t",
+    coordinates: {
+      x: 416,
+      y: 570,
+    },
+    positionCoordinates: {
+      x: 678,
+      y: 538,
+    },
+    setPos:
+      "setpos 1728.944458 295.970032 1676.000000;setang -11.114330 -174.272110 0.000000",
+  },
 ] satisfies UtilityLineup[];
